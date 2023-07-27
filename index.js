57fc66a52a20b6427840ae63e25383e560e0dbc6
suhailsola <suhailsola@hotmail.com>
2023-07-27 14:24:27 +0800

    add portfolio

---
+++ index.js
@@ -0,0 +1,60 @@
+const nodemailer = require("nodemailer");
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/pages/index.html"));
+});
+
+app.get("/thank-you", (req, res) => {
+  res.send("Thank you");
+});
+
+app.get("/error", (req, res) => {
+  res.send("Error");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+app.post("/contact", (req, res) => {
+  // get form data
+  const formData = req.body;
+  // create new transport
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "2f2932c8cfd469",
+      pass: "22cc2feb6602fd",
+    },
+  });
+
+  // create async func to send new email
+  async function sendEmail() {
+    // send mail with defined transport object
+    const info = await transport.sendMail({
+      from: '"suhailsola@hotmail.com', // sender address
+      to: formData.email, // list of receivers
+      subject: `Welcome ${formData.name}`, // Subject line
+      text: JSON.stringify(formData), // plain text body
+      html: `<p>${JSON.stringify(formData)}</p>`, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+  }
+
+  sendEmail()
+    .then(function () {
+      res.redirect("/thank-you");
+    })
+    .catch(function () {
+      res.redirect("/error");
+    });
+});
---
+++ public/script.js
@@ -0,0 +1,49 @@
+console.log("js is loaded");
+
+var checkBox = document.querySelectorAll('input[type="checkbox"]');
+console.log(checkBox);
+
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow((slidePosition += n));
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow((slidePosition = n));
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("photos");
+  if (n > slides.length) {
+    slidePosition = 1;
+  }
+  if (n < 1) {
+    slidePosition = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slidePosition - 1].style.display = "flex";
+}
+
+function closeOtherCheckBox() {}
